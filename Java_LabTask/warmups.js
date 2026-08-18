ad9763a662e28880ec2fa3d9ae2bff938d98cccc
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-08-18 15:03:42 +0400

    Declare	const ,  Declarea num , let , reassign variable twice , 	Operators	&	Comparisons , if for 	falsy	value

---
+++ Java_LabTask/warmups.js
@@ -0,0 +1,24 @@
+
+
+const name = "Asila" // Declare	const for my name
+
+let num = 0 // Declare	a num , let mean can reassign variable twice
+
+// Reassign	variable	twice
+console.log(typeof num); // number
+
+num = "Hi";
+console.log(typeof num); // string
+
+
+console.log("10" == 10);  // true , becuaue it go for the value 10 even if the type deiiferent
+console.log("10" === 10); // false , it goes for the type 
+
+
+const message = "";  // it be falsy because the string is null
+
+if (!mesage) {
+console.log("The message is empty");
+}
+
+
