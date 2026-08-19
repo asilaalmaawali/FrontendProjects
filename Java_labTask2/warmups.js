b05c913fa54a81220904dbb8664754a4c57503d0
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-08-19 15:37:59 +0400

    remove by using pop

--- Java_labTask2/warmups.js
+++ Java_labTask2/warmups.js
@@ -2,3 +2,6 @@ const cities = ["Muscat", "Salalah" , "Nizwa" , "Sohar" ,"Sur" , "Ibri"];
 
 cities.push("Seeb");  // add another city in the end of array
 console.log(cities);
+
+cities.pop();  // remove from the end 
+
