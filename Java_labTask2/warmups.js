0dc6a32b5a6c408057fde879d773fb527156fdef
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-08-19 15:41:51 +0400

    reomve from the beginning by using shift , adding at the first by using unshift

--- Java_labTask2/warmups.js
+++ Java_labTask2/warmups.js
@@ -3,5 +3,8 @@ const cities = ["Muscat", "Salalah" , "Nizwa" , "Sohar" ,"Sur" , "Ibri"];
 cities.push("Seeb");  // add another city in the end of array
 console.log(cities);
 
-cities.pop();  // remove from the end 
+cities.pop();  // remove from the end ("seeb")
 
+cities.shift(); // reomve from the beginning ("Muscat")
+
+cities.unshift("Barka"); // adding at the first "Barka"
