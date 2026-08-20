c2be50560f42048c7325b1a0e1923fd0cf7bffe7
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-08-20 15:24:33 +0400

    Classes

--- Java_labTask3/warmups.js
+++ Java_labTask3/warmups.js
@@ -15,3 +15,30 @@ const usingNullish = discount ?? 10;
 
 console.log(usingOR);      // 10
 console.log(usingNullish); // 0
+
+//2.2	—	Classes
+//for inhertance
+class Employee { constructor(name, role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    introduce() {return `Hi, I am ${this.name}. I am a ${this.role}.`;}
+}
+
+class Manager extends Employee {
+    constructor(name, role, teamSize) {
+        super(name, role);
+        this.teamSize = teamSize;
+    }
+
+    introduce() {
+        return `Hi, I am ${this.name}. I manage ${this.teamSize} people.`;
+    }
+}
+
+const staffMember = new Employee("Sara", "Developer");
+const teamManager = new Manager("Ahmed", "Manager", 10);
+
+console.log(staffMember.introduce());
+console.log(teamManager.introduce());
